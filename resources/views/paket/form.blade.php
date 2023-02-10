28af36193f50f4c4f6e0dd86b0b15fcf320fc8f5
YusranMaulana <120350022+YusranMaulana@users.noreply.github.com>
2023-02-10 16:47:40 +0700

    Buat form,index,edit paket

---
+++ resources/views/paket/form.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.master')
+@section('judul', 'hair artis')
+@section('content-header')
+
+    <section class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1>paket</h1>
+            </div>
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">Blank Page</li>
+              </ol>
+            </div>
+          </div>
+        </div><!-- /.container-fluid -->
+      </section>
+@endsection
+@section('content')
+<section class="content">
+
+    <!-- Default box -->
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title">Title</h3>
+
+        <div class="card-tools">
+          <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+            <i class="fas fa-minus"></i>
+          </button>
+          <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+            <i class="fas fa-times"></i>
+          </button>
+        </div>
+      </div>
+
+      <div class="card-body">
+
+        <form method="POST" action="/paket">
+            @csrf
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Nama</label>
+              <input type="text" name="nama" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">email</label>
+              <input type="text" name="email" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Paketan</label>
+              <select name="paket1_id" class="form-control" id="">
+                <option value="">-Pilih Jurusan-</option>
+                @foreach($paket as $data)
+                <option value="{{$data->id}}">{{$data->kode}} - {{$data->paket}}</option>
+                @endforeach
+              </select>
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">tanggal boking</label>
+              <input type="date" name="tgl_pembokingan" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">bokingan</label>
+              <input type="text" name="pembokingan" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">harga</label>
+              <input type="text" name="pembayaran" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Tambah Data</button>
+          </form>
+      </div>
+      <!-- /.card-body -->
+
+    </div>
+    </section>
+    @endsection
