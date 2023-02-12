05c55b49746874103c628e2cee40dd6913b0e506
YusranMaulana <120350022+YusranMaulana@users.noreply.github.com>
2023-02-13 00:00:38 +0700

    ubah copy an dari paket

--- resources/views/klien/edit.blade.php
+++ resources/views/klien/edit.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.master')
-@section('judul', 'hair artis')
+@section('judul', 'edit klien')
 @section('content-header')
 
     <section class="content-header">
         <div class="container-fluid">
           <div class="row mb-2">
             <div class="col-sm-6">
-              <h1>klien</h1>
+              <h1>Edit klien</h1>
             </div>
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-right">
@@ -38,34 +38,35 @@
 
       <div class="card-body">
 
-        <form method="POST" action="/klien">
+        <form method="POST" action="/klien/{{$klien->id}}">
+            @method('PUT')
             @csrf
             <div class="mb-3">
               <label for="exampleInputEmail1" class="form-label">Nama</label>
-              <input type="text" name="nama" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <input type="text" name="nama" VALUE="{{$klien->nama}}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
             </div>
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">email</label>
-              <input type="text" name="email" class="form-control" id="exampleInputPassword1">
+              <input type="text" name="email" VALUE="{{$klien->email}}" class="form-control" id="exampleInputPassword1">
             </div>
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">paket</label>
-              <input type="number" name="paket" class="form-control" id="exampleInputPassword1">
+              <input type="number" name="paket" VALUE="{{$klien->paket}}" class="form-control" id="exampleInputPassword1">
             </div>
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">tanggal boking</label>
-              <input type="date" name="tgl_pembokingan" class="form-control" id="exampleInputPassword1">
+              <input type="date" name="tgl_pembokingan" VALUE="{{$klien->tgl_boking}}" class="form-control" id="exampleInputPassword1">
             </div>
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">bokingan</label>
-              <input type="text" name="pembokingan" class="form-control" id="exampleInputPassword1">
+              <input type="text" name="pembokingan" VALUE="{{$klien->pembokingan}}" class="form-control" id="exampleInputPassword1">
             </div>
             <div class="mb-3">
               <label for="exampleInputEmail1" class="form-label">harga</label>
-              <input type="text" name="pembayaran" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <input type="text" name="pembayaran" VALUE="{{$klien->pembayaran}}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
             </div>
             
-            <button type="submit" class="btn btn-primary">Tambah Data</button>
+            <button type="submit" class="btn btn-primary">Edit Data</button>
           </form>
       </div>
       <!-- /.card-body -->
